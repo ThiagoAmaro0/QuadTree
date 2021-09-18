148c109fb6dccd4d512d215ddd9b587a5eb3df07
Thiago_Amaro <thiagoamaro010399@gmail.com>
2021-09-18 18:57:45 -0300

    Ajustes

--- QuadTree/src/Main.java
+++ QuadTree/src/Main.java
@@ -1,9 +1,11 @@
+import java.awt.Point;
 
 public class Main {
-
+public static Point canvasSize = new Point(600 ,600);
+	static Canvas canvas;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		canvas = new Canvas(canvasSize);
 	}
 
 }
